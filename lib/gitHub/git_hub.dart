003741cbc494b39f432c8a68d03dffacf4b42de6
imran <imranpathan6305@gmail.com>
2024-03-19 15:29:43 +0530

    Creating a file

---
+++ lib/gitHub/git_hub.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class git_hub extends StatelessWidget {
+  const git_hub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
